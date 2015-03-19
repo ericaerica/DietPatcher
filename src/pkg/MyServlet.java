32fc080fae6abc77a2b88eb281eb3f10ed1732c2
S.F. Ovkaric <stanko16@hotmail.com>
2015-03-19 14:24:59 +0100

    Implemented method doPost in MyServlet.java, postgresql driver jar taken out of the project.

--- src/pkg/MyServlet.java
+++ src/pkg/MyServlet.java
@@ -1,39 +1,94 @@
 package pkg;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MyServlet
- */
-@WebServlet("/MyServlet")
-public class MyServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MyServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+public class MyServlet extends HttpServlet {       
+
+	
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().write("GET/POST response");
+		//Setting up the DB Connection
+		
+		Connection connection = null;
+		Statement st = null;
+        ResultSet rs = null;
+        String output = "";
+        
+		try {//Try driver
+			Class.forName("org.postgresql.Driver");
+ 
+		} catch (ClassNotFoundException e){
+			
+			System.err.println("MISSING PostgreSQL JDBC Driver");
+			e.printStackTrace();
+			return;
+		}
+ 
+		
+
+		try {//Try connection
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/mydb", "postgres",
+					"furantsu16!");
+ 
+		} catch (SQLException e) {
+ 
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+ 
+		}
+ 
+		if (connection != null) {//If you made it
+			
+			try {
+				
+				st = connection.createStatement();
+				rs = st.executeQuery("SELECT * FROM cities");
+				 System.out.println("I'M HEddddEEEEEEEEE");
+				if (rs.next()) {
+	                output+=rs.getString(1)+"<\br>";
+	            }
+				
+			} catch (SQLException e) {
+				System.err.println("ERROR in the query!");
+				e.printStackTrace();
+			}
+            
+			
+		} else {
+			System.err.println("Failed to make connection!");
+		}
+		
+		//Setting up the output
+		
+				PrintWriter writer = response.getWriter();
+				writer.println("<html>"+output+"</html>");
+				writer.flush();
+		
 	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
+		
+
+	}
+
+
 
 }
