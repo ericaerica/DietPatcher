024d272b01271da1d5531aacfc3fa9a391ce22e5
S.F. Ovkaric <stanko16@hotmail.com>
2015-05-09 01:29:56 +0200

    Bean modified and trying to make connection with hibernate

--- src/database/DataManager.java
+++ src/database/DataManager.java
@@ -5,8 +5,33 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import model.UserBean;
 
 public class DataManager {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new AnnotationConfiguration()
+            		.configure()
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+	public static boolean saveUser(UserBean user){
+		
+		return true;
+	}
 
 	/*
  		//Setting up the DB Connection
--- src/model/UserBean.java
+++ src/model/UserBean.java
@@ -1,38 +1,97 @@
 package model;
 
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name="User")
 public class UserBean {
-	//TODO correggere in modo che ci siano solo username, password, email e controllarli con un autenticatore
-	private String usernameLogin, password, usernameReg, email;
+	//TODO controllarli con un autenticatore
+	@Id
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="username")
+	private String usernameLogin;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="gender")
+	private String gender;
+	
+	@Column(name="age")
+	private String age;
+	
+	@Column(name="height")
+	private String height;
+	
+	@Column(name="weight")
+	private String weight;
+	
+	@Column(name="waist")
+	private String waist;
 
 	public String getUsernameLogin() {
 		return usernameLogin;
 	}
-
 	public void setUsernameLogin(String usernameLogin) {
 		this.usernameLogin = usernameLogin;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public String getUsernameReg() {
-		return usernameReg;
-	}
-
-	public void setUsernameReg(String usernameReg) {
-		this.usernameReg = usernameReg;
-	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	public String getWaist() {
+		return waist;
+	}
+
+	public void setWaist(String waist) {
+		this.waist = waist;
 	}	
+	
+	
 }
+
